3cba5f8cfcf13f4d6c85d808b179de9aaeb38ff6
stasszczesniak <stas.szczesniak@gmail.com>
2016-01-30 16:15:50 +0100

    Cleaned and extended delete tests.

--- tests/delete_testing.py
+++ tests/delete_testing.py
@@ -20,8 +20,8 @@ import logging
 import random
 import uuid
 
+from werkzeug.test import Headers
 from flask_testing import TestCase
-
 from bbws import db
 
 
@@ -49,47 +49,26 @@ class DeleteTests(TestCase):
 
         actual_list = \
             db.session.query(self.get_specific_name('entity_class')).all()
-        initial_list = [[x.entity_gid, x.last_updated] for x in actual_list]
-        initial_size = len(initial_list)
-        initial_list.sort()
 
-        while len(actual_list) > 0:
-            rand_pos = random.randint(0, len(actual_list) - 1)
-            random_instance = actual_list[rand_pos]
+        while actual_list:
+            self.delete_random_entity(actual_list)
+            self.check_if_not_deleted(actual_list)
 
-            response_ws = self.client.delete(
-                '/{entity_type}/{entity_gid}/'.format(
-                    entity_type=self.get_specific_name('ws_name'),
-                    entity_gid=random_instance.entity_gid),
-                headers=self.get_request_default_headers(),
-                data="{\"revision\": {\"note\": \"A Test Note\"}}")
+    def delete_random_entity(self, entity_list):
+        random_instance = random.choice(entity_list)
+        response = self.correct_delete_request(random_instance.entity_gid)
 
-            self.assert200(response_ws)
+        self.assert200(response)
+        entity_list.remove(random_instance)
 
-            instances_db_after = \
-                db.session.query(self.get_specific_name('entity_class')).all()
-            self.assertEquals(len(instances_db_after), initial_size)
+    def check_if_not_deleted(self, entities):
+        instances = \
+            db.session.query(self.get_specific_name('entity_class')).all()
+        instances_by_id = dict([(x.entity_gid, x) for x in instances])
 
-            instances_db_after.sort(key=lambda element: element.entity_gid)
-
-            for i in range(len(instances_db_after)):
-                self.assertEquals(
-                    initial_list[i][0],
-                    instances_db_after[i].entity_gid
-                )
-
-                if initial_list[i][0] == random_instance.entity_gid:
-
-                    self.assertIsNone(
-                        instances_db_after[i].master_revision.entity_data
-                    )
-
-                    self.assertGreaterEqual(
-                        instances_db_after[i].last_updated,
-                        initial_list[i][1]
-                    )
-
-            del actual_list[rand_pos]
+        for entity in entities:
+            entity_from_db = instances_by_id[entity.entity_gid]
+            self.assertIsNotNone(entity_from_db.master_revision.entity_data)
 
     def bad_delete_tests(self):
         logging.info(
@@ -99,11 +78,12 @@ class DeleteTests(TestCase):
 
         logging.info(' Bad test #{}'.format(1))
 
-        self.bad_delete_uuid_tests()
-        self.bad_delete_format_tests()
-        self.bad_delete_double_tests()
+        self.bad_uuid_delete_tests()
+        self.bad_format_delete_tests()
+        self.bad_double_delete_tests()
+        self.bad_authentication_delete_tests()
 
-    def bad_delete_uuid_tests(self):
+    def bad_uuid_delete_tests(self):
         instances_db = \
             db.session.query(self.get_specific_name('entity_class')).all()
         initial_size = len(instances_db)
@@ -126,67 +106,69 @@ class DeleteTests(TestCase):
                 )
             self.assertEquals(len(instances_db), initial_size)
 
-    def bad_delete_format_tests(self):
-        instances_db = \
+    def bad_format_delete_tests(self):
+        instances = \
             db.session.query(self.get_specific_name('entity_class')).all()
-        self.assertGreater(len(instances_db), 0)
-        rand_pos = random.randint(0, len(instances_db) - 1)
-        random_instance = instances_db[rand_pos]
+        random_instance = random.choice(instances)
 
-        self.bad_delete_format_single(
-            '/creature/{ent_gid}/'
-            .format(
-                ent_gid=unicode(random_instance.entity_gid)),
-            404)
+        self.bad_format_delete_tests_404(random_instance)
 
-        self.bad_delete_format_single(
-            '//////{ent_gid}'
-            .format(
-                ent_gid=unicode(random_instance.entity_gid)),
-            404)
-
-        self.bad_delete_format_single(
-            '/{ent_gid}'
-            .format(
-                ent_gid=unicode(random_instance.entity_gid)),
-            404)
-
-        self.bad_delete_format_single(
-            '/{ent_gid}/'
-            .format(
-                ent_gid=unicode(random_instance.entity_gid)),
-            404)
-
-        self.bad_delete_format_single(
+        self.bad_format_delete_single_test(
             '/{ent_type}/'
             .format(
                 ent_type=self.get_specific_name('ws_name')),
             405)
 
-        self.bad_delete_format_single(
+        self.bad_format_delete_single_test(
             '/{ent_type}'
             .format(
                 ent_type=self.get_specific_name('ws_name')),
             301)
 
-        self.bad_delete_format_single(
+        self.bad_format_delete_single_test(
             '{ent_type}/{ent_gid}/'
             .format(
                 ent_type=self.get_specific_name('ws_name'),
                 ent_gid=unicode(random_instance.entity_gid)),
-            401
-        )
+            401)
 
-        self.bad_delete_format_single(
+    def bad_format_delete_tests_404(self, random_instance):
+        self.bad_format_delete_single_test(
+            '/creature/{ent_gid}/'
+            .format(
+                ent_gid=unicode(random_instance.entity_gid)),
+            404)
+
+        self.bad_format_delete_single_test(
+            '//////{ent_gid}'
+            .format(
+                ent_gid=unicode(random_instance.entity_gid)),
+            404)
+
+        self.bad_format_delete_single_test(
+            '/{ent_gid}'
+            .format(
+                ent_gid=unicode(random_instance.entity_gid)),
+            404)
+
+        self.bad_format_delete_single_test(
+            '/{ent_gid}/'
+            .format(
+                ent_gid=unicode(random_instance.entity_gid)),
+            404)
+
+        self.bad_format_delete_single_test(
             '{ent_gid}'.format(
                 ent_gid=unicode(random_instance.entity_gid)),
             404)
 
-        self.bad_delete_format_single('/{ent_gid}/{ent_type}/'.format(
-            ent_gid=unicode(random_instance.entity_gid),
-            ent_type=self.get_specific_name('ws_name')), 404)
+        self.bad_format_delete_single_test(
+            '/{ent_gid}/{ent_type}/'.format(
+                ent_gid=unicode(random_instance.entity_gid),
+                ent_type=self.get_specific_name('ws_name')),
+            404)
 
-    def bad_delete_format_single(self, delete_uri, expected_http_status):
+    def bad_format_delete_single_test(self, delete_uri, expected_http_status):
         instances_db = \
             db.session.query(self.get_specific_name('entity_class')).all()
 
@@ -195,25 +177,50 @@ class DeleteTests(TestCase):
 
         instances_db_after = \
             db.session.query(self.get_specific_name('entity_class')).all()
+
         self.assertTrue(len(instances_db), len(instances_db_after))
         for instance in instances_db_after:
-            self.assertNotEquals(instance.master_revision.entity_data, None)
+            self.assertIsNotNone(instance.master_revision.entity_data)
 
-    def bad_delete_double_tests(self):
-        instances_db = \
+    def bad_double_delete_tests(self):
+        instances = \
             db.session.query(self.get_specific_name('entity_class')).all()
-        rand_pos = random.randint(0, len(instances_db) - 1)
-        random_instance = instances_db[rand_pos]
+        random_instance = random.choice(instances)
 
         for i in range(4):
-            response_ws = self.client.delete(
-                '/{entity_type}/{entity_gid}/'.format(
-                    entity_type=self.get_specific_name('ws_name'),
-                    entity_gid=random_instance.entity_gid),
-                headers=self.get_request_default_headers(),
-                data="{\"revision\": {\"note\": \"A Test Note\"}}")
-
+            response = self.correct_delete_request(random_instance.entity_gid)
             if i == 0:
-                self.assert200(response_ws)
+                self.assert200(response)
             else:
-                self.assert405(response_ws)
+                self.assert405(response)
+
+    def bad_authentication_delete_tests(self):
+        instances = \
+            db.session.query(self.get_specific_name('entity_class')).all()
+        random_instance = random.choice(instances)
+
+        headers = Headers(
+            [('Authorization', 'Bearer ' + 'Monty Python'),
+             ('Content-Type', 'application/json')])
+
+        response = self.client.delete(
+            '/{entity_type}/{entity_gid}/'
+            .format(
+                entity_type=self.get_specific_name('ws_name'),
+                entity_gid=random_instance.entity_gid
+            ),
+            headers=headers,
+            data=''
+        )
+        self.assert_status(response, 401)
+
+    def correct_delete_request(self, entity_gid):
+        return self.client.delete(
+            '/{entity_type}/{entity_gid}/'
+            .format(
+                entity_type=self.get_specific_name('ws_name'),
+                entity_gid=entity_gid
+            ),
+            headers=self.get_request_default_headers(),
+            data="{\"revision\": {\"note\": \"A Test Note\"}}"
+        )
